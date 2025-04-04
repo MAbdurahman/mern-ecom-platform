5676d44d62cffb2870adcbecc821bacfbcc5ba00
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-04-04 05:21:22 -0500

    Update frontend/* for daisyui@5.012

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,14 @@
+import {Fragment} from 'react';
+
+
+export default function App() {
+
+   return (
+      <Fragment>
+         <button className="btn btn-primary">One</button>
+         <button className="btn btn-secondary">Two</button>
+         <button className="btn btn-primary btn-outline">Three</button>
+      </Fragment>
+
+   );
+}
